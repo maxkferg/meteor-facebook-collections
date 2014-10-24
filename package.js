b0de54a4f4b944474b2f617e3e0f0d54e217663d
Max Ferguson <maxkferg@gmail.com>
2014-10-24 22:41:21 +0000

    Updated package style to Meteor==0.9.5

--- package.js
+++ package.js
@@ -1,9 +1,12 @@
 Package.describe({
-    summary: "Expose the facebook-graph as a set of meteor collections"
+    summary: "Expose the facebook-graph as a set of meteor collections",
+    version: "0.2.0",
+    git: "https://github.com/maxkferg/meteor-facebook-collections.git"
 });
 
 Package.on_use(function(api) {
-    api.use(['accounts-facebook','http']);
+    api.use('http');
+    api.use('accounts-facebook');
     api.add_files(['facebook-collections.js'], 'client');
     api.export(["FacebookCollections"],'client');
 });
